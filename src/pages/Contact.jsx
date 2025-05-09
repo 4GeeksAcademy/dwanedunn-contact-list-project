c8b7a56241ecaceab3907eeb9433c69c1d90521e
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-09 23:56:36 +0000

    build Contact component and install react-icons

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+function Contact(){
+    return (
+        <section className="contact-wrap">
+            <div className="proifle-image"></div>
+            <div>className="details</div>
+            <div>
+                
+            </div>
+
+        </section>
+    )
+}
+
+export default Contact;
