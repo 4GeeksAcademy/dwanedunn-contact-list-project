18ee8c73c882962e91e37224b839aa22999e5c38
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-10 00:02:51 +0000

    fix contact component jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,19 @@
+import { FaPencil } from "react-icons/fa6";
+import { RiDeleteBin6Fill } from "react-icons/ri";
 function Contact(){
     return (
-        <section className="contact-wrap">
-            <div className="proifle-image"></div>
-            <div>className="details</div>
-            <div>
-                
-            </div>
+        <>
+            <section className="contact-wrap">
+                <div className="proifle-image"></div>
+                <div className="details></div>
+                <div className="contact-icons">
+                    <FaPencil />
+                    <RiDeleteBin6Fill />
 
-        </section>
+                </div>
+
+            </section>
+        </>
     )
 }
 
