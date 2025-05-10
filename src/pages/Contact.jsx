69578efc8618e5c6525ba134025d57666d0279e9
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-10 00:13:52 +0000

    fiix jsx issues

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,19 @@
-import { FaPencil } from "react-icons/fa6";
-import { RiDeleteBin6Fill } from "react-icons/ri";
-function Contact(){
-    return (
-        <>
-            <section className="contact-wrap">
-                <div className="proifle-image"></div>
-                <div className="details></div>
-                <div className="contact-icons">
-                    <FaPencil />
-                    <RiDeleteBin6Fill />
+import { Link } from 'react-router-dom';
+import useGlobalReducer from '../hooks/useGlobalReducer'; // Custom hook for accessing the global state.
 
-                </div>
-
-            </section>
-        </>
-    )
-}
-
-export default Contact;
+// Importing icons from react-icons library
+import { FaPencil } from 'react-icons/fa6';
+import { RiDeleteBin6Fill } from 'react-icons/ri';
+// Contact component
+export const Contact = () => {
+  return (
+    <div className="contact-wrap">
+      <div className="proifle-image"></div>
+      <div className="details"></div>
+      <div className="icon-actions">
+        <FaPencil />
+        <RiDeleteBin6Fill />
+      </div>
+    </div>
+  );
+};
